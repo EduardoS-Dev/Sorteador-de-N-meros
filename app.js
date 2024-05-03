a8b58329fa721c256c6bbc838188f59dde86a2d7
Eduardo Soares <eduardo.soaressilva@icloud.com>
2024-05-02 21:39:57 -0300

    Modificando o botao sortear e a lógica dos valores inseridos

--- app.js
+++ app.js
@@ -6,31 +6,47 @@ function sortear(){
     let sorteados = [];
     let numero;
 
-    for(let i = 0; i < quantidade; i++){
-        numero = obterNumeroAleatorio(doNumero, ateNumero);
-
-        while(sorteados.includes(numero)){
-            numero = obterNumeroAleatorio(doNumero, ateNumero);
+    if(doNumero > ateNumero){
+        alert(`O valor ${doNumero} é maior que o valor ${ateNumero}.`)
+    } else {
+        if(quantidade > (ateNumero - doNumero) + 1){
+            alert('Digite uma quantidade menor para ser sorteado.')
+        } else {
+            for(let i = 0; i < quantidade; i++){
+                numero = obterNumeroAleatorio(doNumero, ateNumero);
+        
+                while(sorteados.includes(numero)){
+                    numero = obterNumeroAleatorio(doNumero, ateNumero);
+                }
+        
+                sorteados.push(numero)
+            }
+        
+            let resultado = document.getElementById('resultado');
+            resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`
+        
+            alterarStatusBotao();  
         }
-
-        sorteados.push(numero)
+        
     }
 
-    let resultado = document.getElementById('resultado');
-    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`
-
-    alterarStatusBotao();
+    
 
 }
 
 function alterarStatusBotao(){
-    let botao = document.getElementById('btn-reiniciar');
-    if(botao.classList.contains('container__botao-desabilitado')){
-        botao.classList.remove('container__botao-desabilitado');
-        botao.classList.add('container__botao');
+    let botao_reiniciar = document.getElementById('btn-reiniciar');
+    let botao_sortear = document.getElementById('btn-sortear');
+    if(botao_reiniciar.classList.contains('container__botao-desabilitado')){
+        botao_reiniciar.classList.remove('container__botao-desabilitado');
+        botao_reiniciar.classList.add('container__botao');
+        botao_sortear.classList.remove('container__botao');
+        botao_sortear.classList.add('container__botao-desabilitado')
     } else{
-        botao.classList.remove('container__botao');
-        botao.classList.add('container__botao-desabilitado');
+        botao_reiniciar.classList.remove('container__botao');
+        botao_reiniciar.classList.add('container__botao-desabilitado');
+        botao_sortear.classList.remove('container__botao-desabilitado');
+        botao_sortear.classList.add('container__botao');
     }
 }
 
@@ -44,7 +60,9 @@ function reiniciar(){
 }
 
 function obterNumeroAleatorio(min, max){
-    return Math.floor(Math.random() * (max - min) + min);
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
 }
 
 
